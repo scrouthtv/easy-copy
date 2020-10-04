05980c80cc3bba5860c4ea727a38e0f1bbc47c4e
Lenni vH <lennivh24@gmail.com>
2020-10-04 16:57:18 +0200

    fixes to the build script

--- build.sh
+++ build.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-SRCDIR=$(dirname $0)/src
-BINLOC=$(dirname $0)/pkg/easycopy
-PKGDIR=$(dirname $0)/pkg
-OUTDIR=$(dirname $0)/build
+ROOTDIR="$(realpath $(dirname $0))"
+echo "root is $ROOTDIR"
+SRCDIR="$ROOTDIR/src"
+BINLOC="$ROOTDIR/pkg/easycopy"
+PKGDIR="$ROOTDIR/pkg"
+OUTDIR="$ROOTDIR/build"
+VERSION="$(grep 'EASYCOPY_VERSION' $SRCDIR/meta.go | grep -oE '[0-9]+.[0-9]+.[0-9]+')"
+OUTPREFIX="easycopy-$VERSION"
+
+mkdir -p "$ROOTDIR" "$SRCDIR" "$PKGDIR" "$OUTDIR"
+rm -rf "$OUTDIR/"*
 
 WIN_ARCHES=("386" amd64)
 LINUX_ARCHES=("386" amd64)
-DARWIN_ARCHES=("386" amd64 arm)
+DARWIN_ARCHES=("386" amd64 arm arm64)
+
+cd "$SRCDIR"
 for GOARCH in ${WIN_ARCHES[@]}; do
-	echo "compiling for windows $GOARCH"
-	GOOS=windows go build -o "$BINLOC-windows-$GOARCH.exe" "$SRCDIR"
-	zip "$OUTDIR/windows-$GOARCH.zip" "$PKGDIR/"*
+	echo "compiling for windows $GOARCH to $BINLOC-windows-$GOARCH.exe"
+	GOOS=windows go build -o "$BINLOC-windows-$GOARCH.exe" .
+	zip -j "$OUTDIR/$OUTPREFIX-windows-$GOARCH.zip" "$PKGDIR/"*
+	rm "$BINLOC-windows-$GOARCH.exe"
 done
 for GOARCH in ${LINUX_ARCHES[@]}; do
 	echo "compiling for linux $GOARCH"
-	GOOS=linux go build -o "$BINLOC-linux-$GOARCH" "$SRCDIR"
-	zstd -o "$OUTDIR/windows-$GOARCH.zip" "$PKGDIR/"*
+	GOOS=linux go build -o "$BINLOC-linux-$GOARCH" .
+	zstd -v -o "$OUTDIR/$OUTPREFIX-linux-$GOARCH.zstd" "$PKGDIR/"*
+	rm "$BINLOC-linux-$GOARCH"
 done
 for GOARCH in ${DARWIN_ARCHES[@]}; do
 	echo "compiling for darwin $GOARCH"
-	GOOS=darwin go build -o "$BINLOC-darwin-$GOARCH.app" "$SRCDIR"
-	zip "$OUTDIR/linux-$GOARCH.zip" "$PKGDIR/"*
+	GOOS=darwin go build -o "$BINLOC-darwin-$GOARCH.app" .
+	zip -j "$OUTDIR/$OUTPREFIX-darwin-$GOARCH.zip" "$PKGDIR/"*
+	rm "$BINLOC-darwin-$GOARCH.app"
 done
+
+cd "$ROOTDIR"
