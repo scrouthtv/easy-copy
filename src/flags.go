aae0d5e32fdec13249228b6bd7c12dd995f6647f
Lenni vH <lennivh24@gmail.com>
2020-10-04 10:03:33 +0200

    reading toml config files using viper

--- src/flags.go
+++ src/flags.go
@@ -3,17 +3,52 @@ package main
 import "fmt"
 import "os"
 
-var verbose bool = false;
+import "github.com/spf13/viper"
 
+var verbose bool;
 var onExistingFile uint8 = 2;
 // 0 skip
 // 1 overwrite
-// 2 ask every time
-// 3 
+// 2 ask
+var followSymlinks uint8 = 1;
+// 0 ignore symlinks
+// 1 follow symlinks, copying them as links
+// 2 fully dereference
 
 func verboseFlags() {
-	fmt.Println("Verbose: ", verbose);
-	fmt.Println("Overwrite Mode: ", onExistingFile);
+	fmt.Println("Verbose:", verbose);
+	fmt.Println("Overwrite Mode:", onExistingFile);
+	fmt.Println("Follow symlinks:", followSymlinks);
+}
+
+func readConfig() {
+	viper.SetDefault("verbose", false);
+	viper.SetDefault("overwrite", 2);
+	viper.SetDefault("followSymlinks", 1);
+
+	viper.AddConfigPath("$XDG_CONFIG_DIR/");
+	viper.AddConfigPath("$XDG_CONFIG_DIR/ec/");
+	viper.AddConfigPath("$HOME/.config/");
+	viper.AddConfigPath("$HOME/.config/ec/");
+	viper.SetConfigName("ec");
+	viper.SetConfigType("toml");
+	err := viper.ReadInConfig();
+	if err != nil {
+		warnConfig(err);
+	}
+	viper.WriteConfig();
+
+	verbose = viper.GetBool("verbose");
+	onExistingFile = uint8(viper.GetInt("overwrite"));
+	followSymlinks = uint8(viper.GetInt("followSymlinks"));
+	if onExistingFile > 2 {
+		warnBadConfig("overwrite", onExistingFile, "0, 1, 2");
+		onExistingFile = 2;
+	}
+	if followSymlinks > 2 {
+		warnBadConfig("followSymlinks", followSymlinks, "0, 1, 2");
+		followSymlinks = 2;
+	}
 }
 
 func parseFlag(prefix string, flag string) {
--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,8 @@
 module easycopy
 
 go 1.15
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/spf13/viper v1.7.1
+)
