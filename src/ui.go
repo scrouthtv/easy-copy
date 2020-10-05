6dfeb218acecffec61a37aecc179c7064fb2b813
Lenni vH <lennivh24@gmail.com>
2020-10-05 19:15:38 +0200

    removed lock logs

--- src/ui.go
+++ src/ui.go
@@ -85,7 +85,6 @@ func verbTargets() {
 		fmt.Print(FGColors.Yellow);
 		fmt.Println("-------------------------");
 		fmt.Println("these tasks will be done:");
-		fmt.Println("[ui:89] lock");
 		filesLock.RLock();
 		var v string;
 		for _, v = range folders {
@@ -95,7 +94,6 @@ func verbTargets() {
 			var target string = targets[v];
 			fmt.Println(v, "will be copied to", target + "/");
 		}
-		fmt.Println("[ui:99] unlock");
 		filesLock.RUnlock();
 		fmt.Println("-------------------------");
 		fmt.Print(Textstyle.Reset);
