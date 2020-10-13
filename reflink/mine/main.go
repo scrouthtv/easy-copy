0d698a6448c95ad8512df1f8f2a2cba9fbb0b51f
Lenni vH <lennivh24@gmail.com>
2020-10-13 08:36:37 +0200

    testing

--- reflink/mine/main.go
+++ reflink/mine/main.go
@@ -3,7 +3,7 @@ package main;
 import "fmt";
 
 func main() {
-	var err error = reflink("/mnt2/src", "/mnt2/reflink");
+	var err error = reflink("/mnt2/notfile", "/mnt2/myreflink");
 	if err == nil {
 		fmt.Println("Done.");
 	} else {
