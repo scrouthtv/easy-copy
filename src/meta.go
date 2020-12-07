7da7e2ded43f1a62fad28e56b5fd0f6f71ef1780
Lenni vH <lennivh24@gmail.com>
2020-12-07 19:12:20 +0100

    push to v0.3.0

--- src/meta.go
+++ src/meta.go
@@ -4,7 +4,7 @@ import "path/filepath"
 import "os"
 
 const EASYCOPY_NAME string = "EasyCopy"
-const EASYCOPY_VERSION string = "0.2.5"
+const EASYCOPY_VERSION string = "0.3.0"
 const EASYCOPY_ISSUES string = "https://github.com/scrouthtv/easy-copy"
 
 // these ones are also hardcoded in the manpage
