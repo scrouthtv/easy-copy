0dbc321bb6717d631aae4ba23c34a48f6bfa671c
scrouthtv <lennivh24@gmail.com>
2021-07-13 12:15:02 +0200

    Update fs.go

--- mockfs/fs.go
+++ mockfs/fs.go
@@ -1,5 +1,14 @@
 package mockfs
 
+import "io/fs"
+
 type MockFS struct {
 	Root *MockFolder
+}
+
+// MockEntry groups all information about a file.
+type MockEntry interface {
+	File
+	fs.FileInfo
+	fs.DirEntry
 }
